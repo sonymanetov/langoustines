f96aba2e090410f79126bd67468bcba31ff34534
BigMama <sonymanetov@mail.ru>
2021-01-24 19:25:05 +0300

    Прикручивание таблицы

--- api/api.py
+++ api/api.py
@@ -1,8 +1,34 @@
 import time
 from flask import Flask
+import requests
+from xml.dom import minidom
+
+# стоимость за кг
+SHRIMP_VALUE = 919
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+
+@app.route('/exchange_rate')
+def get_exchange_rate():
+    crb_response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+
+    response = {'items':[]}
+    response_item = {'name':'Российский рубль', 'code': 'RUB', 'value': round(1/SHRIMP_VALUE*1000, 2)}
+    response['items'].append(response_item)
+    xmldoc = minidom.parseString(crb_response.text)
+    itemlist = xmldoc.getElementsByTagName('Valute')
+    for item in itemlist:
+        response_item = {}
+        response_item['name'] = item.getElementsByTagName('Name')[0].firstChild.nodeValue
+        response_item['code'] = item.getElementsByTagName('CharCode')[0].firstChild.nodeValue
+        nominal = float(item.getElementsByTagName('Nominal')[0].firstChild.nodeValue)
+        value = item.getElementsByTagName('Value')[0].firstChild.nodeValue
+        value = float(value.replace(',', '.'))
+        response_item['value'] = round(value/nominal/SHRIMP_VALUE*1000, 2)
+        response['items'].append(response_item)
+    return response
--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import './App.css';
 import shrimp from './assets/shrimpWo4ki.png';
 
 function App() {
-    const [currentTime, setCurrentTime] = useState(0);
+    const [exchangeRates, setExchangeRates] = useState([]);
 
       useEffect(() => {
-        fetch('/time').then(res => res.json()).then(data => {
-          setCurrentTime(data.time);
+        fetch('/exchange_rate').then(res => res.json()).then(data => {
+          setExchangeRates(data.items);
         });
       }, []);
+
+          const listItems = exchangeRates.map((rate) =>
+            <p>{rate.name} ({rate.code}) - {rate.value} грамм</p>
+          );
 
       return (
         <div className="App">
           <header className="App-header">
           <img src={shrimp} className="Shrimp" alt="shrimp" />
-            <p>The current time is {currentTime}.</p>
+                <div>{listItems}</div>
           </header>
         </div>
       );
