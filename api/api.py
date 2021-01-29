27e78d122d8f5c7072e62a64064b980f00f92c2a
BigMama <sonymanetov@mail.ru>
2021-01-29 21:49:51 +0300

    Работа часовщика

--- api/api.py
+++ api/api.py
@@ -2,23 +2,25 @@ import time
 from flask import Flask
 import requests
 from xml.dom import minidom
+import datetime
+import pytz
 
 # стоимость за кг
 SHRIMP_VALUE = 919
-DATE = '24/01/21'
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
-
+def format_date(date, month, year, hour=None, minute=None):
+    res = '/'.join((str(date).zfill(2), str(month).zfill(2), str(year).zfill(2)[-2:]))
+    if hour and minute:
+        res = res + ' ' + ':'.join((str(hour).zfill(2), str(minute).zfill(2)))
+    return res
 
 @app.route('/exchange_rate')
 def get_exchange_rate():
     crb_response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
 
-    response = {'items':[], 'cost':SHRIMP_VALUE, 'date':DATE}
+    response = {'items':[], 'cost':SHRIMP_VALUE, 'date': format_date(24, 1, 21)}
     response_item = {'name':'Российский рубль', 'code': 'RUB', 'value': round(1/SHRIMP_VALUE*1000, 2)}
     response['items'].append(response_item)
     xmldoc = minidom.parseString(crb_response.text)
@@ -32,4 +34,10 @@ def get_exchange_rate():
         value = float(value.replace(',', '.'))
         response_item['value'] = round(value/nominal/SHRIMP_VALUE*1000, 2)
         response['items'].append(response_item)
+
+    val_curs = xmldoc.getElementsByTagName('ValCurs')[0]
+    response['crb_date'] = format_date(*(val_curs.getAttribute('Date').split('.')))
+    moscow_tz = pytz.timezone('Europe/Moscow')
+    current_date = datetime.datetime.now(tz = moscow_tz)
+    response['update_date'] = format_date(current_date.day, current_date.month, current_date.year, current_date.hour, current_date.minute)
     return response
--- src/App.js
+++ src/App.js
@@ -5,13 +5,34 @@ import shrimp from './assets/shrimpWo4ki.png';
 function App() {
     const [exchangeRates, setExchangeRates] = useState([]);
     const [langCost, setLangCost] = useState(0);
-    const [langDate, setLangDate] = useState(0);
+    const [langDate, setLangDate] = useState('');
+    const [crbDate, setCrbDate] = useState('');
+    const [updateDate, setUpdateDate] = useState('');
+
+    function FormatDate(date_str) {
+        let fMonth = ['января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря'];
+        var split_str = date_str.split(' ');
+        var date = split_str[0].split('/');
+        var day = date[0];
+        var month = fMonth[parseInt(date[1])-1];
+        var year = '20' + date[2];
+        var result = day + ' ' + month + ' ' + year;
+        if (split_str.length > 1) {
+            var time = split_str[1].split(':');
+            var hour = time[0];
+            var minute = time[1];
+            result = hour + ':' + minute + ' ' + result;
+        }
+            return result;
+    }
 
       useEffect(() => {
         fetch('/exchange_rate').then(res => res.json()).then(data => {
           setExchangeRates(data.items);
           setLangCost(data.cost);
           setLangDate(data.date);
+          setCrbDate(FormatDate(data.crb_date));
+          setUpdateDate(FormatDate(data.update_date));
         });
       }, []);
 
@@ -34,6 +55,8 @@ function App() {
                 </thead>
                 <tbody>{listItems}</tbody>
               </table>
+              <div><p> Курс получен на {crbDate}. Данные актуальны на {updateDate} года. Источник: <a href="https://www.cbr.ru">www.cbr.ru </a></p>
+              </div>
               <div className="Remark" > <hr/> <p>* Значение рассчитывается по средней статистической оценке <span className = "ValueDate">({langCost} руб/кг на {langDate})</span> рыночной стоимости лангустин категории L1. Средний вес лангустины считать за 100 грамм. Ура креветки! </p> </div>
               <p className="Credits" > Сделал <a href="https://github.com/sonymanetov">Леха Манетов</a> (почти сам) </p>
             </div>
